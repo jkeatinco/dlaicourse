a5715c5b0c459384d4a7063f5d57cf2df6cb293d
Jeff Keating <jkeatinco@gmail.com>
2020-03-21 16:44:16 -0700

    week 4 update

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 2/Exercise/fashion-script_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 2/Exercise/fashion-script_exercise.js
@@ -20,8 +20,6 @@ function getModel() {
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
     model.add(tf.layers.conv2d({filters: 16, kernelSize: 3, activation: 'relu'}));
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
-    model.add(tf.layers.conv2d({ filters: 32, kernelSize: 3, activation: 'relu' }));
-    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
     model.add(tf.layers.flatten());
     model.add(tf.layers.dense({units: 128, activation: 'relu'}));
     model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
